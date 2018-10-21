88c8605d6ba093d1e212f74a13809a0a11db8c4d
Hoang Viet <vieritolove@gmail.com>
2018-10-21 23:50:26 +0700

    test

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ class DataGenerator(Sequence):
 
         # for x,y in temp:
         #     print(x.shape, y.shape)
-        return [x[0] for x in temp], [x[1] for x in temp]
+        return np.array([x[0] for x in temp]), np.array([x[1] for x in temp])
 
 
 model = Unet(backbone_name='resnet50', encoder_weights='imagenet', freeze_encoder=True)
