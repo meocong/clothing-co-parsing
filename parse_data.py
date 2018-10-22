3b2237b51c1cb4f5759e6be46586aebc13bdc932
Hoang Viet <vieritolove@gmail.com>
2018-10-22 20:42:15 +0700

    parse data

---
+++ parse_data.py
@@ -0,0 +1,33 @@
+import glob
+import scipy.io
+import cv2
+import numpy as np
+import random
+import os
+
+pixel_level_files = glob.glob("./real_mask/*.png")
+
+for mask in pixel_level_files:
+    photo = mask.replace("real_mask", "photos").replace("png","jpg")
+
+    img = cv2.imread(photo)
+    imask = cv2.imread(mask)
+
+    img_name = photo.split("/")[-1].replace("jpg","png")
+    if (random.randint(0,100) < 5):
+        cv2.imwrite("original_data/test/" + img_name, img)
+        cv2.imwrite("original_data/val/" + img_name, img)
+        cv2.imwrite("original_data/test_labels/" + img_name.replace("png","_L.png"), imask)
+        cv2.imwrite("original_data/val_labels/" + img_name.replace("png","_L.png"), imask)
+        # os.system("cp {0} {1}".format(photo, "original_data/test/" + img_name))
+        # os.system("cp {0} {1}".format(photo, "original_data/val/" + img_name))
+        #
+        # os.system("cp {0} {1}".format(mask, "original_data/test_labels/" + img_name.replace("png","_L.png")))
+        # os.system("cp {0} {1}".format(mask, "original_data/val_labels/" + img_name.replace("png","_L.png")))
+    else:
+        # os.system("cp {0} {1}".format(photo, "original_data/train/" + img_name))
+        # os.system("cp {0} {1}".format(mask, "original_data/train_labels/" + img_name.replace("png","_L.png")))
+
+        cv2.imwrite("original_data/train/" + img_name, img)
+        cv2.imwrite("original_data/train_labels/" + img_name.replace("png", "_L.png"),imask)
+        pass
--- process_data.py
+++ process_data.py
@@ -2,14 +2,54 @@ import glob
 import scipy.io
 import cv2
 import numpy as np
+import random
 
-pixel_level_files = glob.glob("./annotations/image-level/*.mat")
+pixel_level_files = glob.glob("./annotations/pixel-level/*.mat")
+
+# Process label
+import pandas as pd
+
+mat = [x[0] for x in scipy.io.loadmat("label_list.mat")['label_list'][0]]
+result = [["null",0,0,0],]
+
+r = [51*x for x in range(0,6)]
+g = [51*x for x in range(0,6)]
+b = [51*x for x in range(0,6)]
+
+random.shuffle(r)
+random.shuffle(g)
+random.shuffle(b)
+
+u = 0
+v = 0
+c = 1
+for x in mat[1:]:
+    result.append([x,r[u],g[v],b[c]])
+
+    if (c == 5 and v == 5):
+        u += 1
+        v = 0
+        c = 0
+    elif (c == 5):
+        v += 1
+        c = 0
+    else:
+        c += 1
+
+df = pd.DataFrame(data={"name":[x[0] for x in result],
+               "r": [x[1] for x in result],
+               "g": [x[2] for x in result],
+               "b": [x[3] for x in result]})
+
+df.to_csv("class_dict.csv", header=True,columns=["name","r","g","b"], index=False)
 
 for x in pixel_level_files:
     mat = scipy.io.loadmat(x)
     mat = np.array(mat['groundtruth'])
-    # cv2.imshow("img", mat)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
 
-    cv2.imwrite("./mask/{0}".format(x.split("/")[-1].replace(".mat", ".jpg")), mat)
+    image = np.zeros((mat.shape[0], mat.shape[1], 3))
+
+    for uu in range(len(result)):
+        image[np.where(mat == uu)] = (result[uu][1], result[uu][2], result[uu][3])
+
+    cv2.imwrite("./real_mask/" + x.split("/")[-1].replace(".mat", ".png"), image)
